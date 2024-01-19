2305a69310ae2eaa4e88d41a078fe18861738c3f
handamn <kupretzzz14@gmail.com>
2024-01-19 14:00:19 +0700

    Only modify decision 1

--- Program/Main_Program.py
+++ Program/Main_Program.py
@@ -8,11 +8,14 @@ BASIS_FOLDER = "F:\\repo_generator\\V1\\data_generator\\"
 print("1. new project")
 print("2. load project")
 
+grup = []
+
 lokasi = []
 steer = []
 box = []
 tipe = []
 
+x =100
 
 decision = input("Masukkan Menu : ")
 if int(decision) == 1:
@@ -22,56 +25,16 @@ if int(decision) == 1:
     ####################
     nama_project = input("Masukkan Nama Project : ")
     print("")
-    #os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project)
-    ####################
-    jumlah_lokasi = input("Masukkan jumlah lokasi pengambilan gambar : ")
-    for i in range(0, int(jumlah_lokasi)):
-        nama_lokasi = input("Masukkan Nama Lokasi Gambar " + str(i+1) + " : ")
-        lokasi.append(nama_lokasi)
 
-        #os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi)
-        print("")
-    print("")
-    
-    jumlah_steer = input("Masukkan jumlah posisi steer : ")
-    for j in range(0, int(jumlah_steer)):
-        nama_steer = input("masukkan Nama Steer " + str(j+1) + " : ")
-        steer.append(nama_steer)
-        #os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_steer)
-    print("")
+    jumlah_grup = input("Masukkan jumlah lokasi pengambilan gambar : ")
+    for i in range(0,int(jumlah_grup)):
+        nama_grup = input("masukkan Nama Grup " + str(i+1) + " : ")
+        grup.append(nama_grup)
 
-    jumlah_box = input("Masukkan jumlah box : ")
-    for k in range(0, int(jumlah_box)):
-        nama_box = input("masukkan Nama Box " + str(k+1) + " : ")
-        box.append(nama_box)
-        print("")
-    print("")
-    
-    jumlah_tipe = input("Masukkan jumlah tipe kendaraan : ")
-    for l in range(0, int(jumlah_tipe)):
-        nama_tipe = input("masukkan Nama Tipe " + str(l+1) + " : ")
-        tipe.append(nama_tipe)
-        print("")
-    print("")
+    for i in range(0,len(grup)):
+        print(1)
 
 
-    print(lokasi)
-    print("")
-    print(steer)
-    print("")
-    print(box)
-    print("")
-    print(tipe)
-    print("")
-
-
-    ####################
-
-
-
-
-    #os.mkdir(BASIS_FOLDER+"Project\\"+nama_folder)
-    print("")
 
 
 
