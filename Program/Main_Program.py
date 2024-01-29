542aba7b7e0f9798e9fb2195a18c286fc4c7af1f
handamn <kupretzzz14@gmail.com>
2024-01-29 14:39:33 +0700

    Combine the nested loop and previous way program

--- Program/Main_Program.py
+++ Program/Main_Program.py
@@ -1,52 +1,65 @@
 import os
 
+BASIS_FOLDER = "F:\\repo_generator\\V1\\data_generator\\Project\\"
+
+def create_folders(folder_name, folder_depths, folder_names, current_depth=0):
+    
+    if current_depth == len(folder_depths) - 1:
+        last_subfolder_names = []
+        for i in range(folder_depths[current_depth]):
+            num_subfolders = int(input(f"Masukkan jumlah subfolder untuk folder {folder_names[current_depth][i]}: "))
+            names = []
+            for j in range(num_subfolders):
+                name = input(f"Masukkan nama subfolder {j+1} untuk folder {folder_name[len(BASIS_FOLDER):]} {folder_names[current_depth][i]}: ")
+                names.append(name)
+            last_subfolder_names.append(names)
+        
+        for i in range(folder_depths[current_depth]):
+            subfolder_name = os.path.join(BASIS_FOLDER, folder_name, folder_names[current_depth][i])
+            os.makedirs(subfolder_name)  # Membuat folder
+            
+            for name in last_subfolder_names[i]:
+                os.makedirs(os.path.join(subfolder_name, name))
+                os.makedirs(os.path.join(subfolder_name, name, "images"))
+                os.makedirs(os.path.join(subfolder_name, name, "labels"))
+                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi"))
+                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi", "images"))
+                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi", "labels"))
+    
+    elif current_depth < len(folder_depths) - 1:  # Periksa apakah sudah mencapai kedalaman terakhir
+        for i in range(folder_depths[current_depth]):
+            subfolder_name = os.path.join(BASIS_FOLDER, folder_name, folder_names[current_depth][i])
+            os.makedirs(subfolder_name)  # Membuat folder
+            create_folders(subfolder_name, folder_depths, folder_names, current_depth + 1)
+
 # Clear screen command
 os.system('cls' if os.name == 'nt' else 'clear')
 
-BASIS_FOLDER = "F:\\repo_generator\\V1\\data_generator\\"
+# Inisialisasi list kosong
+folder_names = []
 
 print("1. new project")
 print("2. load project")
 
-
 decision = input("Masukkan Menu : ")
 if int(decision) == 1:
-    ####################
     print("Buat Project")
     print("")
     ####################
     nama_project = input("Masukkan Nama Project : ")
     print("")
-    os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project)
-    ####################
-    jumlah_lokasi = input("Masukkan jumlah lokasi pengambilan gambar : ")
-    print("")
-    jumlah_steer = input("Masukkan jumlah posisi steer : ")
-    print("")
-    jumlah_box = input("Masukkan jumlah box : ")
-    print("")
-    jumlah_tipe = input("Masukkan jumlah tipe kendaraan : ")
-    print("")
-    for i in range(0, int(jumlah_lokasi)):
-        nama_lokasi = input("Masukkan Nama Lokasi Gambar " + str(i+1) + " : ")
-        print("")
-        for j in range(0, int(jumlah_steer)):
-            nama_steer = input("masukkan Nama Steer " + str(j+1) + ", " + nama_lokasi +" : ")
-            print("")
-            for k in range(0, int(jumlah_box)):
-                print("")
-                nama_box = input("masukkan Nama Box " + str(k+1) + ", " + nama_lokasi + ", " + nama_steer + " : ")
-                for l in range(0, int(jumlah_tipe)):
-                    print("")
-                    nama_tipe = input("Masukkan Nama Tipe " + str(l+1) +  ", " + nama_lokasi + ", " + nama_steer + ", " + nama_box + " : ")
-                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi)
-                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer)
-                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer + "\\" + nama_box)
-                    os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer + "\\" + nama_box + "\\" + nama_tipe)
 
-    ####################
+    kedalaman = input("Masukkan Kedalaman Folder : ")
+    isi_kedalaman = []
+    for i in range(0, int(kedalaman)):
+        item = int(input(f"Masukkan jumlah subfolder untuk tingkat kedalaman {i+1}: "))
+        isi_kedalaman.append(item)
 
+        subfolders = []
+        for j in range(item):
+            subfolder_name = input(f"Masukkan nama subfolder {j+1} untuk folder {nama_project}: ")
+            subfolders.append(subfolder_name)
+        
+        folder_names.append(subfolders)        
 
-
-else :
-    print("List Project :")
+    create_folders(nama_project, isi_kedalaman, folder_names)
