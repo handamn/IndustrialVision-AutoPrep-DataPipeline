ee7a6e4ca8836df889aa982117e5ddb290285baa
handamn <kupretzzz14@gmail.com>
2024-01-29 14:38:38 +0700

    Use the nested loop

--- Program/Main_Program.py
+++ Program/Main_Program.py
@@ -29,22 +29,22 @@ if int(decision) == 1:
     print("")
     for i in range(0, int(jumlah_lokasi)):
         nama_lokasi = input("Masukkan Nama Lokasi Gambar " + str(i+1) + " : ")
-        os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi)
         print("")
-
-    for j in range(0, int(jumlah_steer)):
-        nama_steer = input("masukkan Nama Steer " + str(j+1) + " : ")
-        os.mkdir(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_steer)
-
-
+        for j in range(0, int(jumlah_steer)):
+            nama_steer = input("masukkan Nama Steer " + str(j+1) + ", " + nama_lokasi +" : ")
+            print("")
+            for k in range(0, int(jumlah_box)):
+                print("")
+                nama_box = input("masukkan Nama Box " + str(k+1) + ", " + nama_lokasi + ", " + nama_steer + " : ")
+                for l in range(0, int(jumlah_tipe)):
+                    print("")
+                    nama_tipe = input("Masukkan Nama Tipe " + str(l+1) +  ", " + nama_lokasi + ", " + nama_steer + ", " + nama_box + " : ")
+                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi)
+                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer)
+                    #os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer + "\\" + nama_box)
+                    os.makedirs(BASIS_FOLDER + "Project" + "\\" + nama_project + "\\" + nama_lokasi + "\\" + nama_steer + "\\" + nama_box + "\\" + nama_tipe)
 
     ####################
-
-
-
-
-    #os.mkdir(BASIS_FOLDER+"Project\\"+nama_folder)
-    print("")
 
 
 
