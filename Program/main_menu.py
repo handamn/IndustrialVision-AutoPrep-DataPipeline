6a145e46aefaa023d14eae453bf1e29a84767419
handamn <kupretzzz14@gmail.com>
2024-01-30 22:47:43 +0700

    Change the way of program main_menu.py

--- Program/main_menu.py
+++ Program/main_menu.py
@@ -1,36 +1,26 @@
 import os
+import shutil
+import Start4_folder_V1 as foldering
+
+def print_menu():
+    print("\nProgram Generator\n")
+    print("Pilih Menu :")
+    print("1. Ambil Gambar")
+    print("2. Pilih Random 50")
+    print("3. Anotasi")
+    print("4. Training Prepare Image")
+    print("5. Auto Anotasi")
+    print("6. Combine")
+    print("7. Training Final\n")
+
+    hasil_menu    = input("Enter Menu                                       : ")
+    # Clear screen command
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("======================================================")
+
+    return hasil_menu
 
 BASIS_FOLDER = "F:\\repo_generator\\V1\\data_generator\\Project\\"
-
-def create_folders(folder_name, folder_depths, folder_names, current_depth=0):
-    
-    if current_depth == len(folder_depths) - 1:
-        last_subfolder_names = []
-        for i in range(folder_depths[current_depth]):
-            num_subfolders = int(input(f"Masukkan jumlah subfolder untuk folder {folder_names[current_depth][i]}: "))
-            names = []
-            for j in range(num_subfolders):
-                name = input(f"Masukkan nama subfolder {j+1} untuk folder {folder_name[len(BASIS_FOLDER):]} {folder_names[current_depth][i]}: ")
-                names.append(name)
-            last_subfolder_names.append(names)
-        
-        for i in range(folder_depths[current_depth]):
-            subfolder_name = os.path.join(BASIS_FOLDER, folder_name, folder_names[current_depth][i])
-            os.makedirs(subfolder_name)  # Membuat folder
-            
-            for name in last_subfolder_names[i]:
-                os.makedirs(os.path.join(subfolder_name, name))
-                os.makedirs(os.path.join(subfolder_name, name, "images"))
-                os.makedirs(os.path.join(subfolder_name, name, "labels"))
-                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi"))
-                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi", "images"))
-                os.makedirs(os.path.join(subfolder_name, name, "X_Automasi", "labels"))
-    
-    elif current_depth < len(folder_depths) - 1:  # Periksa apakah sudah mencapai kedalaman terakhir
-        for i in range(folder_depths[current_depth]):
-            subfolder_name = os.path.join(BASIS_FOLDER, folder_name, folder_names[current_depth][i])
-            os.makedirs(subfolder_name)  # Membuat folder
-            create_folders(subfolder_name, folder_depths, folder_names, current_depth + 1)
 
 # Clear screen command
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -62,4 +52,28 @@ if int(decision) == 1:
         
         folder_names.append(subfolders)        
 
-    create_folders(nama_project, isi_kedalaman, folder_names)
+    foldering.create_folders(os.path.join(BASIS_FOLDER, nama_project), isi_kedalaman, folder_names, BASIS_FOLDER)
+
+elif int(decision) == 2:
+    dir_list = os.listdir(BASIS_FOLDER) 
+
+    print("Files and directories in '", BASIS_FOLDER, "' :") 
+
+    # print the list 
+    print(dir_list)
+
+    print("")
+    
+    pilih_folder = input("Masukkan Nama Project : ")
+    print("Saat ini sedang di project " + pilih_folder)
+
+    BASIS_FOLDER = "F:\\repo_generator\\V1\\data_generator\\Project\\" + pilih_folder + "\\"
+
+    print(BASIS_FOLDER)
+
+    pilih_menu = print_menu()
+
+    if pilih_menu == "1":
+        print("UHUYYYY")
+
+
