a589688a664fc506381771cf8885a90d17bfb8b0
handamn <kupretzzz14@gmail.com>
2024-01-31 23:07:43 +0700

    Add data_input_default.py

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -1,1 +1,9 @@
 #this file is basis for all function
+def data_input_default():
+    car           = input("Enter Car Model           (ex : Innova)          : ")
+    steer         = input("Enter Steer               (ex : RHD)             : ")
+    box           = input("Enter Box Class           (ex : Box1)            : ")
+    kode_box      = input("Enter Code Box            (ex : X_3_CDE)         : ")
+    kendaraan = car + "_" + steer
+
+    return kendaraan, box, kode_box
