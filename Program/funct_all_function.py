3d5df46defa3c8e9ce18576ef11064dcb3cd5aa8
handamn <kupretzzz14@gmail.com>
2024-02-01 23:45:10 +0700

    Simplify baca_file and data_input_default

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -1,30 +1,30 @@
 #this file is basis for all function
 
-
 file_path = "F:\\repo_generator\\V1\\data_generator\\Project\\RB24\\group.txt"
 
-
 ##### baca_file function
-def baca_file(file_path):
-    with open(file_path, 'r') as file:
+def baca_file(route):
+    with open(route, 'r') as file:
         lines = [line.strip() for line in file.readlines()]  # Menghapus karakter whitespace dari setiap baris
     return lines
 
-#print(baca_file(file_path))
+def data_input_default(route):
+    list_var = baca_file(route)
+    dict_value_input = {}
 
-#print(len(baca_file(file_path)))
+    for i in range(len(list_var)):
+        value = input("Masukkan Value untuk " + list_var[i] + " : ")
+        dict_value_input[i] = value
+
+    return dict_value_input
 
 
-tes = baca_file(file_path)
 
 
-tesss = {}
+tes = data_input_default(file_path)
 
-for i in range(len(tes)):
-    key = tes[i]
-    value = input("Masukkan Value untuk " + tes[i] + " : ")
-    tesss[key] = value
+print(len(tes))
 
-print(tesss)
+print(tes[0])
 
 ###############################################
