90e11d5a91336994802c76da1ed7f079743217e9
handamn <kupretzzz14@gmail.com>
2024-02-01 23:26:08 +0700

    Add baca_file

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -1,1 +1,30 @@
 #this file is basis for all function
+
+
+file_path = "F:\\repo_generator\\V1\\data_generator\\Project\\RB24\\group.txt"
+
+
+##### baca_file function
+def baca_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = [line.strip() for line in file.readlines()]  # Menghapus karakter whitespace dari setiap baris
+    return lines
+
+#print(baca_file(file_path))
+
+#print(len(baca_file(file_path)))
+
+
+tes = baca_file(file_path)
+
+
+tesss = {}
+
+for i in range(len(tes)):
+    key = tes[i]
+    value = input("Masukkan Value untuk " + tes[i] + " : ")
+    tesss[key] = value
+
+print(tesss)
+
+###############################################
