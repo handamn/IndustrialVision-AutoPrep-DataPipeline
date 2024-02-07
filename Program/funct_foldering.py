1c8a026d073955431afbb8177cac1766f23c0502
handamn <kupretzzz14@gmail.com>
2024-02-07 13:32:24 +0700

    Add list_all_subfolders_with_longest

--- Program/funct_foldering.py
+++ Program/funct_foldering.py
@@ -4,6 +4,26 @@ import shutil
 import yaml
 import subprocess
 import sys
+import psutil
+import time
+
+def list_all_subfolders_with_longest(path):
+    subfolders = []
+    max_depth = 0
+    deepest_path = None
+    for root, dirs, _ in os.walk(path):
+        for dir_name in dirs:
+            dir_path = os.path.join(root, dir_name)
+            subfolders.append(dir_path)
+            # Update informasi tentang subfolder terdalam
+            depth = len(os.path.relpath(dir_path, path).split(os.sep))
+            if depth > max_depth:
+                max_depth = depth
+                deepest_path = dir_path
+
+    parsed_deepest_path = deepest_path.split(os.sep)
+    
+    return subfolders, parsed_deepest_path
 
 def create_folders(folder_name, folder_depths, folder_names, BASE_FOLDER, current_depth=0, is_folder_A=True):
     if current_depth == 0:  # Menambahkan pembuatan folder nama project, 1_Stock_Photo, dan 2_Train_Artefact
@@ -93,24 +113,11 @@ def duplicate_structure(source_folder, target_folder):
             os.makedirs(os.path.join(root, "test", "labels"))
             os.makedirs(os.path.join(root, "models"))
 
+    all_subfolders, deepest_path = list_all_subfolders_with_longest(target_folder)
 
-def list_all_subfolders_with_longest(path):
-    subfolders = []
-    max_depth = 0
-    deepest_path = None
-    for root, dirs, _ in os.walk(path):
-        for dir_name in dirs:
-            dir_path = os.path.join(root, dir_name)
-            subfolders.append(dir_path)
-            # Update informasi tentang subfolder terdalam
-            depth = len(os.path.relpath(dir_path, path).split(os.sep))
-            if depth > max_depth:
-                max_depth = depth
-                deepest_path = dir_path
-
-    parsed_deepest_path = deepest_path.split(os.sep)
-    
-    return subfolders, parsed_deepest_path
+    print("==================")
+    print(deepest_path)
+    print("==================")
 
 
 def copy_folder(name_project, group_len, index_group):
@@ -141,25 +148,6 @@ def copy_folder(name_project, group_len, index_group):
     delete_route = base_route
     delete_route += "\\" + "3_Base"
 
+    # Menyalin folder
     shutil.copytree(copy_route, dest_route)
-    #shutil.rmtree(delete_route)
-
-
-def cut_folder(name_project):
-    script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-    clean_script = script_directory.split('\\')
-    base_route = ""
-    for i in range(len(clean_script)):
-        if clean_script[i] != "Program":
-            if (i==0):
-                base_route = clean_script[i]
-            
-            else :
-                base_route += "\\" + clean_script[i]
-    
-    base_route += "\\" + "Project" + "\\" + name_project
-
-    delete_route = base_route
-    delete_route += "\\" + "3_Base"
-
     shutil.rmtree(delete_route)
