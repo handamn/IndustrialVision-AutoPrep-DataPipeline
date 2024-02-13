ae0b559ac0c234da54fda3e9b3f3e2b35b6932ce
handamn <kupretzzz14@gmail.com>
2024-02-13 14:22:32 +0700

    Update funct_all_function for camera function

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -30,49 +30,46 @@ def data_input_default(route):
 ###############################################
 
 ##### ##### collections of cameras function
-fe_path = "F:\\repo_generator\\V1\\data_generator\\Project\\RB24\\group.txt"
-list_of_input = read.data_input_default(fe_path)
+def capture(route_path):
 
-jumlah_gambar = int(input("Enter How Many Image      (ex : 100)             : "))
+    group_route = route_path + "group.txt"
+    image_route = route_path + "1_Stock_Photo"
 
-basis_folder = "F:\\repo_generator\\V1\\data_generator\\Project\\RB24\\1_Stock_Photo"
+    list_of_input = read.data_input_default(group_route)
+    image_count = int(input("Enter How Many Image      (ex : 100)             : "))
 
-simpan_folder = basis_folder
+    for i in range(len(list_of_input)):
+        image_route += "\\" + list_of_input[i]
 
-for i in range(len(list_of_input)):
-    simpan_folder += "\\" + list_of_input[i]
+    image_route += "\\imag es" 
 
-simpan_folder += "\\images"
+    cap = cv2.VideoCapture(0)
 
-number_images = jumlah_gambar
+        # Looping image save program
+    for imgnum in range(image_count):
+        print('Collecting image {}'.format(imgnum))
+        ret, frame = cap.read()
 
-cap = cv2.VideoCapture(0)
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")[:-3]
 
-# Looping image save program
-for imgnum in range(number_images):
-    print('Collecting image {}'.format(imgnum))
-    ret, frame = cap.read()
+        imgname = os.path.join(image_route, f'{timestamp}.jpg')
+        cv2.imwrite(imgname,frame)
+        cv2.imshow('frame',frame)
+        time.sleep(0.0001)
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")[:-3]
+        if cv2.waitKey(1)&0xFF ==ord('q'):
+            break
 
-    imgname = os.path.join(simpan_folder, f'{timestamp}.jpg')
-    cv2.imwrite(imgname,frame)
-    cv2.imshow('frame',frame)
-    time.sleep(0.0001)
-
-    if cv2.waitKey(1)&0xFF ==ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
-print("")
-print("======================================================================================================")
-print("")
-print("----------------------------------------AMBIL GAMBAR BERHASIL-----------------------------------------")
-print("")
-print("------------------------------------------Gambar disimpan di------------------------------------------")
-print("")
-print(simpan_folder)
-print("")
-print("======================================================================================================")
+    cap.release()
+    cv2.destroyAllWindows()
+    print("")
+    print("======================================================================================================")
+    print("")
+    print("----------------------------------------AMBIL GAMBAR BERHASIL-----------------------------------------")
+    print("")
+    print("------------------------------------------Gambar disimpan di------------------------------------------")
+    print("")
+    print(image_route)
+    print("")
+    print("======================================================================================================")
 ############################ End of Collections
