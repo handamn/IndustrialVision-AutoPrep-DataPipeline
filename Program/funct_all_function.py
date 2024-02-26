6b17fab5289354d6814cf0d7d89822ad183d12f9
handamn <kupretzzz14@gmail.com>
2024-02-26 10:57:24 +0700

    Trying function coba2

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -80,23 +80,16 @@ def data_input_default2(route, decision):
             if list_var[i] == list_var_crop[j]:
                 dict_value_input_crop[j] = value
     
-    print("")
-    print(list_var)
-    print("")
+    """print("")
     print(val_var)
     print("")
-    print(list_var_crop)
-    print("")
     print(val_crop)
-    print("")
-    print(dict_value_input)
-    print("")
-    print(dict_value_input_crop)
+    print("")"""
     
     return dict_value_input, dict_value_input_crop
 
 
-data_input_default2("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\", "key")
+#data_input_default2("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\", "key")
 
 
 def simple_route2(main_route):
@@ -119,8 +112,21 @@ def simple_route2(main_route):
         if i < (len(crop_list_of_input)-1):
             mod_input_result_route += crop_list_of_input[i] + "\\"
 
+    #print(og_base_route)
+    #print(og_automate_route)
+    #print(code)
+    #print(input_result_route)
+    #print(mod_input_result_route)
+    #print(og_list_of_input)
+    #print("")
+    #print(crop_list_of_input)
 
     return og_base_route, og_automate_route, code, input_result_route, mod_input_result_route
+
+#simple_route2("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\")
+
+#simple_route2("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\","complete")
+
 
 
 def csv_file_reader(nama_file):
@@ -150,7 +156,13 @@ def split_data(route_path, decision):
     data_csv = csv_file_reader(class_file)
     output = route_path + "2_Train_Artefact"
 
+    #print(class_file)
+    #print(output)
+    #print("")
+
     get_code = parse_base_route[-1]
+    #print(get_code)
+    #print(data_csv)
     
     for code in data_csv:
         print(code)
@@ -179,6 +191,9 @@ def coba2(route_path):
     data_csv = csv_file_reader(class_file)
     output = route_path + "2_Train_Artefact"
 
+    print("")
+    print(route_path)
+    print("")
     print(base_route)
     print("")
     print(automate_route)
@@ -188,3 +203,17 @@ def coba2(route_path):
     print(input_result_route)
     print("")
     print(mod_input_result_route)
+
+    #for code in data_csv:
+        #for second_main in 
+        #print(code)
+
+
+coba2("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\")
+
+#coba("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\", "0")
+#coba("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\", "complete")
+
+
+
+#split_data("F:\\repo_generator\\V1\\data_generator\\Project\\RB1\\", "complete")
