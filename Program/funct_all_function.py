b0ca4107255d6d42df2c011c3ab3966f436d052e
handamn <kupretzzz14@gmail.com>
2024-06-05 00:10:56 +0700

    Merge the develop branch to main

--- Program/funct_all_function.py
+++ Program/funct_all_function.py
@@ -1,12 +1,24 @@
 #this file is basis for all function
 
-##### Function for input default item
-def data_input_default():
-    car           = input("Enter Car Model           (ex : Innova)          : ")
-    steer         = input("Enter Steer               (ex : RHD)             : ")
-    box           = input("Enter Box Class           (ex : Box1)            : ")
-    kode_box      = input("Enter Code Box            (ex : X_3_CDE)         : ")
-    kendaraan = car + "_" + steer
 
-    return kendaraan, box, kode_box
-#################################################
+file_path = "F:\\repo_generator\\V1\\data_generator\\Project\\RB24\\group.txt"
+
+##### baca_file function
+def baca_file(route):
+    with open(route, 'r') as file:
+        lines = [line.strip() for line in file.readlines()]  # Menghapus karakter whitespace dari setiap baris
+    return lines
+###############################################
+
+##### Function for input default item
+def data_input_default(route):
+    list_var = baca_file(route)
+    dict_value_input = {}
+
+    for i in range(len(list_var)):
+        value = input("Masukkan Value untuk " + list_var[i] + " : ")
+        dict_value_input[i] = value
+
+    return dict_value_input
+
+###############################################
